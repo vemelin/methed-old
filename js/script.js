a358059571d49ece53383a2d9073bba6b8ca77c5
vemelin <vemelin@gamil.com>
2022-08-18 14:03:19 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const productName = 'iPhone',
+    productNumber = 200,
+    productType = ['iPhone 13 Pro', 'iPhone 13', 'Phone SE', 'iPhone 12', 'iPhone'],
+    productPrice = 999;
+
+let summary = productNumber * productPrice;
+
+console.log('Название продукта:' + ' ' + productName);
+console.log('Сумма:' + ' ' + summary + '$');
