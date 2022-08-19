d8b892bd06d58aaeeadc0ba6fc843997a9ea0b1d
shaman <shaman@shamans-MacBook-Pro.local>
2022-08-19 00:43:49 -0500

    Module 1, Topic 3 commitment

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 'use strict'
-
-const productName = 'iPhone',
-    productNumber = 200,
-    productType = ['iPhone 13 Pro', 'iPhone 13', 'Phone SE', 'iPhone 12', 'iPhone'],
-    productPrice = 999;
-
-let summary = productNumber * productPrice;
-
 {
     const name = 'iPhone 13 Pro';
     const count = 5;
     const category = 'mobile phone';
     const price = 999;
-
     console.log(name);
     console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+    const name = prompt('Наименование товара?', ''),
+        quantity = + prompt('Количество товара?', ''),
+        category = prompt('Категория товара?', ''),
+        price = + prompt('Цена товара?', '')
+    console.log(`На складе ${quantity} единицы товара "${name}" на сумму ${quantity * price} деревянных`);
 }
